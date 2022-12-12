d92371c8cefeb0b1798e39978df22e90a49065aa
lblake <treleven.lloyd@gmail.com>
2022-12-12 23:47:37 +0000

    refactored code to use arrow functions and added template literals to - ternaries.js code

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,4 +1,4 @@
 // Instructions can be found in ternaries.md
 
 export const sayHello = (name) =>
-  name === undefined ? 'Hello you!' : 'Hello, ' + name + '!';
+  name === undefined ? 'Hello you!':`Hello, ${name}!`;
